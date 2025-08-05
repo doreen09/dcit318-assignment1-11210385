4c4a82f9f8c476ec6e9aee6efb02ace5eec1ed38
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-05 10:35:43 +0000

    Created a new app called the Triangle Type Identifier, which is meant to tell the type of triangle based on the length of its sides

---
+++ TriangleTypeIdentifier/TriangleTypeIdentifier.cs
@@ -0,0 +1,10 @@
+﻿/*
+THE TRIANGLE TYPE IDENTIFIER
+This program that prompts the user to enter three sides of a triangle. Based on the lengths 
+of the sides entered, determine and display the type of the triangle. 
+The possible types are: - 
+Equilateral: All three sides are equal. 
+Isosceles: Two sides are equal. 
+Scalene: No sides are equal. 
+*/
+
