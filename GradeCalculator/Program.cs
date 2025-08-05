6720ba19c463fdc6f71fced7902d3416e2ba7c4d
doreen09 <daowireduaa@st.ug.edu.gh>
2025-08-05 09:36:49 +0000

    Updated GradeCalculator logic with student input

---
+++ GradeCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+/* This program  prompts the user to enter a numerical grade between 0 and 100. Based 
+on the grade entered, it displays the corresponding letter grade using the following scale:
+90 and above: A
+80-89: B 
+70-79: C 
+60-69: D 
+Below 60: F */
+
+class GradeCalculator
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Your Exam Score is: " + Score());
+
+    }
+    static int Score()
+    {
+        Console.Write("Please Enter Your Exam Score: ");
+        string input = Console.ReadLine();
+        int examScore = int.Parse(input);
+        return examScore;
+    }
+}
